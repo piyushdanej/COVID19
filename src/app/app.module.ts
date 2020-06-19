76c1deffbeccda3608344fe8e15a25c542cb9736
AbdAllah El Gamal <abdallah.algamal93@gmail.com>
2020-06-19 15:39:08 +0530

    Delete unnecessary folder inside components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Screen2Component } from './components/screen2/screen2.component';
 import { AvailableCliniciansComponent } from './components/available-clinicians/available-clinicians.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Screen2Component,
     AvailableCliniciansComponent
   ],
   imports: [
