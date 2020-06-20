922f1112387d1b11ca447fa1f8f74f468738e973
PiyushDanej <piyush.danej@gmail.com>
2020-06-20 14:59:14 +0530

    View Screenings almost HTML and components

--- src/app/components/patient-to-call/patient-to-call.component.ts
+++ src/app/components/patient-to-call/patient-to-call.component.ts
@@ -1,14 +1,15 @@
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-patient-to-call',
+  selector: 'patient-to-call',
   templateUrl: './patient-to-call.component.html',
   styleUrls: ['./patient-to-call.component.css']
 })
 export class PatientToCallComponent implements OnInit {
   @Input() patientName : string;
   @Input() patientDate : string;
-
+  faPhone = faPhone;
 
   constructor() { }
 
