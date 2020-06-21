c0db728b4ac0ac5c36f57fb0e6c69dfb121b7eae
AbdAllah El Gamal <abdallah.algamal93@gmail.com>
2020-06-21 17:35:45 +0530

    Patient APIs for Firebase database and patient interface

--- src/app/interfaces/patient.ts
+++ src/app/interfaces/patient.ts
@@ -1,2 +1,13 @@
 export interface Patient {
+    id: string;
+    firstName: string;
+    lastName: string;
+    city: string;
+    state: string;
+    zipCode: string;
+    mobileNumber: string;
+    emailId: string;
+    age: number;
+    sex: string;
+    password: string;
 }
--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Patient } from '../interfaces/patient';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MainService {
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
+
+  getPatients() {
+    return this.firestore.collection('patients').snapshotChanges();
+  }
+
+  createPatient(patient: Patient){
+    return this.firestore.collection('patients').doc(patient.id).set(patient);
+  }
+
+  updatePatient(patient: Patient){
+    delete patient.id;
+    this.firestore.doc('patients/' + patient.id).update(patient);
+  }
+
+  deletePatient(patientId: string){
+    this.firestore.doc('patients/' + patientId).delete();
+  }
 
   getAvailableClinicians(){
     // get request here.
