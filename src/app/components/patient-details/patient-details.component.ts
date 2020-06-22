a8d28a9244f87350b0ae239c14b8124b7ae84a6f
AbdAllah El Gamal <abdallah.algamal93@gmail.com>
2020-06-22 15:26:55 +0530

    Displaying name and age in the profile page

--- src/app/components/patient-details/patient-details.component.ts
+++ src/app/components/patient-details/patient-details.component.ts
@@ -4,6 +4,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
+import { AngularFirestore } from '@angular/fire/firestore';
+
 @Component({
   selector: 'app-patient-details',
   templateUrl: './patient-details.component.html',
@@ -16,7 +18,8 @@ export class PatientDetailsComponent implements OnInit {
   meterList;
   constructor(private router : Router ,
               private route : ActivatedRoute ,
-              private mainService : MainService) { 
+              private mainService : MainService,
+              private firestore: AngularFirestore) { 
                 this.meterList = Array(16).fill(1);
               }
   isTravelHistory:"Yes";
@@ -24,6 +27,10 @@ export class PatientDetailsComponent implements OnInit {
   feedbackDiscription;
 
   faArrowLeft = faArrowLeft;
+  
+  firstName = '';
+  lastName = '';
+  age = '';
 
   ngOnInit() {
     this.isTravelHistory = "Yes";
@@ -31,6 +38,10 @@ export class PatientDetailsComponent implements OnInit {
     this.feedbackDiscription = "";
 
     this.getAllPatients();
+
+    this.firstName = this.route.snapshot.paramMap.get('firstName');
+    this.lastName = this.route.snapshot.paramMap.get('lastName');
+    this.age = this.route.snapshot.paramMap.get('age');
 
   }
   submitSurvey(){
@@ -46,7 +57,7 @@ export class PatientDetailsComponent implements OnInit {
     this.feedback = e.target.value;
   }
   navigateBack(){
-    this.router.navigate(["/screenings"] , { queryParams: { id: 0} , relativeTo : this.route})
+    this.router.navigate(["/screenings"] , { queryParams: { id: 0} , relativeTo : this.route});
   }
   
   selectTab(event){
