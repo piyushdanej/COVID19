52eff31d47d08490074b9a960c48a35094ae1fab
PiyushDanej <piyush.danej@gmail.com>
2020-06-23 20:14:47 +0530

    Typo fixes + removed firstname hard coding from clinicianHome

--- src/app/components/clinician-home/clinician-home.component.ts
+++ src/app/components/clinician-home/clinician-home.component.ts
@@ -1,3 +1,4 @@
+import { MainService } from 'src/app/services/main.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { faChevronRight, faUser, faCog } from '@fortawesome/free-solid-svg-icons';
@@ -13,10 +14,14 @@ export class ClinicianHomeComponent implements OnInit {
   faChevronRight = faChevronRight;
   faUser = faUser;
   faCog = faCog;
+  userDetails : any ={};
 
-  constructor(private router :Router) { }
+  constructor(private mainService : MainService , private router :Router) { }
 
   ngOnInit() {
+    this.userDetails = this.mainService.getLoggedInPatient()
+    console.log(this.userDetails);
+
   }
   routeToViewScreenings(param: number){
    
